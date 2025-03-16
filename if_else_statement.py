7c7a8feb39264fd55e2fcbd3fbd43a452bc1a270
cristinescheiquilay <scheiyaliuq@gmail.com>
2025-03-16 13:25:54 +0800

    Submitting Python Week 04 - Laboratory # 09

---
+++ if_else_statement.py
@@ -0,0 +1,6 @@
+user_input = input("Enter a number: 10 ")
+number = int(user_input)
+if number % 2 == 0:
+    print("The number", number, "is Even.")
+else:
+    print("The number", number, "is Odd.")
